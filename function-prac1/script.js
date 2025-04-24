85200d5504c05e833a7101107c5270b2af3febcc
freezy29 <189191275+freezy29@users.noreply.github.com>
2025-04-24 16:45:59 +0800

    test commit

--- function-prac1/script.js
+++ function-prac1/script.js
@@ -1,17 +1,25 @@
-function add7(num1){
-    return num1 + 7;
+function add7(num1) {
+  return num1 + 7;
 }
 
 let result = add7(5);
 console.log(result);
 
-function multiply(oneNum, twoNum){
-   return oneNum * twoNum;
- }
- let product = multiply(4,4);
- console.log(product);
+function multiply(oneNum, twoNum) {
+  return oneNum * twoNum;
+}
+let product = multiply(4, 4);
+console.log(product);
 
- function capital(){
-    
- }
- 
+function capital(name) {
+  return String(name).charAt(0).toUpperCase() + String(name).slice(1);
+}
+let cap = capital("john");
+console.log(cap);
+
+function lastLetter(last){
+    return String(last).charAt(3);
+}
+let char = lastLetter("abcd");
+console.log(char);
+
